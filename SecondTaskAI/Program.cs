957a183ca22be6382ff466d0a9f4dea1dafac983
Alexey Ustinov <TotKtoSmog513@gmail.com>
2023-10-12 17:33:56 +0300

    Авторизация В ВК

--- SecondTaskAI/Program.cs
+++ SecondTaskAI/Program.cs
@@ -1,10 +1,34 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+
 namespace SecondTaskAI
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private static async Task Main(string[] args)
         {
+            List<string> ListUser = csvHelper.ReadCsvFile(@"data\dataF.csv", "Ваш ID в VK");
+            ListUser = ListUser.Select(item => CorrecterVkId(item)).ToList();
+            VkApi api = await Authorization();
+            Console.ReadKey();
+        }
+        private static string CorrecterVkId(string item) => item.Trim('@').Replace("https://vk.com/", "");
+        private static async Task<VkApi> Authorization()
+        {
+            string AccessToken = await txtHelper.ReadFileAsync(Authorize.getAuthorizeDataPath());
+            VkApi api = new VkApi();
+            api.Authorize(new ApiAuthParams()
+            {
+                AccessToken = AccessToken,
+                Settings = Settings.All
+            });
+            return api;
         }
     }
 }
