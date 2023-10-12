66ce57e18ff3900caf0698d7f7fb4697153f9a98
Alexey Ustinov <TotKtoSmog513@gmail.com>
2023-10-12 15:59:44 +0300

    Добавление класса для работы с текстовыми файлами

---
+++ SecondTaskAI/Model/Authorize.cs
@@ -0,0 +1,9 @@
+﻿namespace SecondTaskAI
+{
+    internal static class Authorize
+    {
+        private static string _pathAuthorizeData = @"data\AuthorizeData.txt";
+        internal static string getAuthorizeDataPath() => _pathAuthorizeData;
+        internal const int AccessToken = 0;
+    }
+}
---
+++ SecondTaskAI/txtHelper.cs
@@ -0,0 +1,49 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SecondTaskAI
+{
+    internal static class txtHelper
+    {
+        internal static async Task<string> ReadFileAsync(string path)
+        {
+            string result;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                result = await reader.ReadToEndAsync();
+            }
+            return result;
+        }
+        internal static async Task<List<string>> ReadFileLinesAsync(string path)
+        {
+            List<string> result = new List<string>();
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+                return null;
+            }
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                    result.Add(line);
+            }
+            return result;
+        }
+
+        internal static async void WriteFileAsync(string path, string text)
+        {
+            using (FileStream fstream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                byte[] buffer = Encoding.Default.GetBytes(text);
+                await fstream.WriteAsync(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
