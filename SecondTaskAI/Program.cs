b2f1a5b125463f9f440b6a279bea19e74ecb9aa4
Alexey Ustinov <TotKtoSmog513@gmail.com>
2023-10-29 20:30:24 +0300

    fix

--- SecondTaskAI/Program.cs
+++ SecondTaskAI/Program.cs
@@ -3,10 +3,6 @@ using SecondTaskAI.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.NetworkInformation;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Security.Policy;
 using System.Threading;
 using System.Threading.Tasks;
 using VkNet;
@@ -112,179 +108,12 @@ namespace SecondTaskAI
         }
         private static async Task Main(string[] args)
         {
-            /*
-            string param = "";
-            int i = -1;
-            int counter = 0;
-            List<string> list = new List<string>();*/
             VkApi api = await Authorization();
-            /*
-            if (!txtHelper.FileExists($@"data\dataUser\lvl1.txt") || !txtHelper.FileExists($@"data\dataUser\lvl2.txt"))
-            {
-                foreach (var vUser in api.Users.Get(csvHelper.ReadCsvFile(@"data\dataF.csv", "Ваш ID в VK")))
-                {
-                    users.Add(new VkApiUser(vUser.Id.ToString()));
-                }
-                
-                while (param != "" || i < users.Count - 1)
-                {
-                    i++;
-                    SenderMessage.sendMessage($"{i + 1}) Пользователь {users[i].me}");
-                    param += $"{users[i].me},";
-                    if (i % 12 == 0 && i != 0)
-                    {
-
-                        int id = param.Split(',').Count() - 1;
-                        var result = GetFriendsID(ref param, api);
-                        for (int j = 0; j < id; j++)
-                        {
-                            foreach (var u in result[j].ToListOf<string>(n => n))
-                                users[counter].friends.Add(new VkApiUser(u));
-                            counter++;
-                        }
-                        Thread.Sleep(310);
-                    }
-                    if (i >= users.Count - 1)
-                    {
-                        int id = param.Split(',').Count() - 1;
-                        var result = GetFriendsID(ref param, api);
-                        for (int j = 0; j < id; j++)
-                        {
-                            foreach (var u in result[j].ToListOf<string>(n => n))
-                                users[counter].friends.Add(new VkApiUser(u));
-                            counter++;
-                        }
-                        Thread.Sleep(310);
-                    }
-
-                }
-                foreach (var u in users)
-                    list.Add(u.me);
-                txtHelper.WriteFileLines($@"data\dataUser\lvl1.txt", list);
-                foreach (var u in users)
-                {
-                    txtHelper.WriteFileLines($@"data\dataUser\lvl2.txt", new List<string>() { "#" + u.me }, true);
-                    list.Clear();
-                    foreach (var f in u.friends)
-                        list.Add(f.me);
-                    txtHelper.WriteFileLines($@"data\dataUser\lvl2.txt", list, true);
-                }
-            }
-            else
-            {
-                list = await txtHelper.ReadFileLinesAsync($@"data\dataUser\lvl1.txt");
-                foreach (var user in list)
-                    users.Add(new VkApiUser(user));
-                list.Clear();
-                list = await txtHelper.ReadFileLinesAsync($@"data\dataUser\lvl2.txt");
-                foreach (var user in list)
-                {
-                    if(user.First() == '#')
-                    {
-                        i++;
-                        continue;
-                    }  
-                    users[i].friends.Add(new VkApiUser(user));
-                }
-                Console.WriteLine();
-            }
-
-            users.Clear();
-            list = await txtHelper.ReadFileLinesAsync($@"data\dataUser\lvl3.txt");
-            i = -1;
-            foreach (var item in list)
-            {
-                if (item.First() == '#')
-                {
-                    users.Add(new VkApiUser(item.Replace("#", "")));
-                    i++;
-                    continue;
-                }
-                else
-                    users[i].friends.Add(new VkApiUser(item));
-            }*/
-           
-
 
             users = await LoadUser($@"data\dataUser\lvl3.txt");
             SenderMessage.sendMessage($"{users.Count}");
             getDataUserInFile(ref users, $@"data\dataUser\lvl5.txt", api);
 
-            /*
-            SenderMessage.sendMessage($"Читаем друзей");
-            if (!txtHelper.FileExists($@"data\dataUser\lvl1.txt"))
-            {
-                foreach (var u in users)
-                {
-                    i = -1;
-                    counter = 0;
-                    param = "";
-
-                    while (param != "" || i < u.friends.Count - 1)
-                    {
-                        i++;
-                        SenderMessage.sendMessage($"{i + 1}) Пользователь {u.friends[i].me}");
-                        param += $"{u.friends[i].me},";
-                        if (i % 12 == 0 && i != 0)
-                        {
-
-                            int id = param.Split(',').Count() - 1;
-                            var result = GetFriendsID(ref param, api);
-
-                            for (int j = 0; j < id; j++)
-                            {
-                                foreach (var item in result[j].ToListOf<string>(n => n))
-                                    u.friends[counter].friends.Add(new VkApiUser(item));
-                                counter++;
-                            }
-                            Thread.Sleep(310);
-                        }
-                        if (i >= u.friends.Count - 1)
-                        {
-                            int id = param.Split(',').Count() - 1;
-                            if (id > 0)
-                            {
-                                var result = GetFriendsID(ref param, api);
-                                for (int j = 0; j < id; j++)
-                                {
-                                    foreach (var item in result[j].ToListOf<string>(n => n))
-                                        u.friends[counter].friends.Add(new VkApiUser(item));
-                                    counter++;
-                                }
-                            }
-                            Thread.Sleep(310);
-                        }
-                    }
-                    foreach (var f in u.friends)
-                    {
-                        txtHelper.WriteFileLines($@"data\dataUser\lvl4.txt", new List<string>() { "#" + f.me }, true);
-                        list.Clear();
-                        foreach (var ff in f.friends)
-                            list.Add(ff.me);
-                        txtHelper.WriteFileLines($@"data\dataUser\lvl4.txt", list, true);
-                    }
-
-
-                }
-            }
-            else
-            {
-                users.Clear();
-                list = await txtHelper.ReadFileLinesAsync($@"data\dataUser\lvl3.txt");
-                i = -1;
-                foreach (var item in list)
-                {
-                    if(item.First() == '#')
-                    {
-                        users.Add(new VkApiUser(item.Replace("#", "")));
-                        i++;
-                        continue;
-                    }
-                    else 
-                        users[i].friends.Add(new VkApiUser(item));
-                }
-            }
-            */
             Console.ReadKey();
         }
 
@@ -295,19 +124,6 @@ namespace SecondTaskAI
             var result = api.Call("execute.getFriendsById", new VkParameters(parameters));
             ids = "";
             return result;
-        }
-        private static void PrintMessage()
-        {
-            string temp = "";
-            while (message != "exit")
-            {
-                if (message == "" || message == temp) continue;
-                else
-                {
-                    temp = message;
-                    Console.WriteLine(message);
-                }
-            }
         }
         private static void sendMessageConsole(string mes, ConsoleColor color = ConsoleColor.White )
         {
@@ -338,47 +154,6 @@ namespace SecondTaskAI
                 SenderMessage.sendErrorMessage($"Возникла ошибка {e.Message}");
             }
             return friend;
-        }
-        private static async void checkIdVk(VkApi api, string id)
-        {
-            sendMessageConsole($"Начало работы с id/псевдонимом {id}",ConsoleColor.Green);
-            var uservk = api.Users.Get(new List<string>() { id });
-            Thread.Sleep(510);
-            if (uservk.Count() > 0)
-            {
-                long idvk = uservk[0].Id;
-                string path = $@"data\dataUser\id{idvk}.txt";
-                paths.Add(path);
-                if (txtHelper.FileExists(path))
-                {
-                    sendMessageConsole($"Файл с пользователем id {id} уже существует", ConsoleColor.Green);
-                    return;
-                }
-                sendMessageConsole($"Запись в файл: {path}");
-                VkCollection<User> friend = await GetVkFriends(api, idvk);
-                if (friend != null)
-                {
-                    sendMessageConsole($"У польователя {id}, числовой id = {idvk}, количество друзей {friend.Count}");
-                    List<string> myFriend = friend.Select(n => $"{idvk}:{n.Id}").ToList();
-                    txtHelper.WriteFileLines(path, myFriend);
-                    int i = 1;
-                    foreach (User user in friend)
-                    {
-                        sendMessageConsole($"\t {i++}) начало работы с другом id {user.Id}");
-                        VkCollection<User> f = await GetVkFriends(api, user.Id);
-                        if (f != null)
-                        {
-                            sendMessageConsole($"\t количестов друзей {f.Count}");
-                            myFriend = f.Select(n => $"{user.Id}:{n.Id}").ToList();
-                            txtHelper.WriteFileLines(path, myFriend, true);
-                        }
-                        Thread.Sleep(510);
-                    }
-                }
-            }
-            else
-                sendMessageConsole($"Пользователь по id {id} не найден", ConsoleColor.Red);
-            sendMessageConsole($"Закончина работа с пользователем {id}",ConsoleColor.Green);  
         }
         private static void joinFile()
         {
